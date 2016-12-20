d3919b0e9c8e6420452c125b52721b353c6d52b9
erikvw <ew2789@gmail.com>
2016-12-20 15:29:44 +0200

    add missing manager

--- plot/managers.py
+++ plot/managers.py
@@ -2,29 +2,20 @@
 
 from django.db import models
 
-from bcpp.manager_mixins import BcppSubsetManagerMixin
 
-
-class PlotManager(BcppSubsetManagerMixin, models.Manager):
-
-    reference_model = 'plot.plot'
-    to_reference_model = ['household_structure', 'household', 'plot']
+class PlotManager(models.Manager):
 
     def get_by_natural_key(self, plot_identifier):
         return self.get(plot_identifier=plot_identifier)
 
 
-class PlotLogManager(BcppSubsetManagerMixin, models.Manager):
-
-    lookup = ['plot']
+class PlotLogManager(models.Manager):
 
     def get_by_natural_key(self, plot_identifier):
         return self.get(plot__plot_identifier=plot_identifier)
 
 
-class PlotLogEntryManager(BcppSubsetManagerMixin, models.Manager):
-
-    lookup = ['plot_log', 'plot']
+class PlotLogEntryManager(models.Manager):
 
     def get_by_natural_key(self, report_datetime, plot_identifier):
         return self.get(report_datetime=report_datetime, plot__plot_identifier=plot_identifier)
