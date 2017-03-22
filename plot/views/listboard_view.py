f26ebe07b37e75cf6135145d56513cd1cca51734
erikvw <ew2789@gmail.com>
2017-03-23 01:00:27 +0200

    add space to listboard, move plot and household filter/exclude in view to mixin

--- plot/views/listboard_view.py
+++ plot/views/listboard_view.py
@@ -1,23 +1,21 @@
-import socket
-
 from django.apps import apps as django_apps
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.conf import settings
 
 from edc_base.view_mixins import EdcBaseViewMixin
 from edc_dashboard.view_mixins import AppConfigViewMixin
 from edc_dashboard.views import ListboardView as BaseListboardView
-from edc_map.models import InnerContainer
 
 
 from ..constants import RESIDENTIAL_HABITABLE
 from ..models import Plot
+from ..view_mixins import PlotQuerysetViewMixin
 
 from .wrappers import PlotWithLogEntryModelWrapper
 
 
-class ListBoardView(AppConfigViewMixin, EdcBaseViewMixin, BaseListboardView):
+class ListBoardView(AppConfigViewMixin, EdcBaseViewMixin,
+                    PlotQuerysetViewMixin, BaseListboardView):
 
     app_config_name = 'plot'
     navbar_item_selected = 'plot'
@@ -35,32 +33,3 @@ class ListBoardView(AppConfigViewMixin, EdcBaseViewMixin, BaseListboardView):
             map_url_name=django_apps.get_app_config('plot').map_url_name,
         )
         return context
-
-    def get_queryset_exclude_options(self, request, *args, **kwargs):
-        options = super().get_queryset_exclude_options(
-            request, *args, **kwargs)
-        plot_identifier = django_apps.get_app_config(
-            'plot').anonymous_plot_identifier
-        options.update({'plot_identifier': plot_identifier})
-        return options
-
-    def get_queryset_filter_options(self, request, *args, **kwargs):
-        options = super().get_queryset_filter_options(request, *args, **kwargs)
-        map_area = settings.CURRENT_MAP_AREA
-        options.update(
-            {'map_area': map_area})
-        plot_identifier = kwargs.get('plot_identifier')
-        device_id = socket.gethostname()[-2:]
-        if plot_identifier:
-            options.update(
-                {'plot_identifier': plot_identifier})
-        plot_identifier_list = []
-        try:
-            plot_identifier_list = InnerContainer.objects.get(
-                device_id=device_id).identifier_labels
-        except InnerContainer.DoesNotExist:
-            plot_identifier_list = []
-        if plot_identifier_list:
-            options.update(
-                {'plot_identifier__in': plot_identifier_list})
-        return options
