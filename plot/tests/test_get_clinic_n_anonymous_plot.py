ab573e32eb603306bf1e130d1f3b1c6630ea7e42
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-06-03 11:22:49 +0200

    add clinic and anonymous plot creation

--- plot/tests/test_get_clinic_n_anonymous_plot.py
+++ plot/tests/test_get_clinic_n_anonymous_plot.py
@@ -1,3 +1,5 @@
+import re
+
 from django.apps import apps as django_apps
 from django.test import TestCase
 
@@ -18,6 +20,20 @@ class TestCreateClinicAnonymousPlot(TestCase):
         plot_identifier = django_apps.get_app_config(
             'plot').clinic_plot_identifiers[0]
         plot_type = 'clinic'
-        get_clinic_n_anonymous_plot(
+        plot = get_clinic_n_anonymous_plot(
             plot_identifier=plot_identifier, plot_type=plot_type)
+        pattern = re.compile("[0-9]{2}[0000-00]")
+        self.assertTrue(pattern.match(plot.plot_identifier))
+        self.assertEqual(Plot.objects.all().count(), 1)
+
+    def test_create_anonymous_plot(self):
+        """Test creating a clinic plot.
+        """
+        plot_identifier = django_apps.get_app_config(
+            'plot').anonymous_plot_identifier
+        plot_type = 'anonymous'
+        plot = get_clinic_n_anonymous_plot(
+            plot_identifier=plot_identifier, plot_type=plot_type)
+        pattern = re.compile("[0-9]{2}[1-9]{2}[00-00]")
+        self.assertTrue(pattern.match(plot.plot_identifier))
         self.assertEqual(Plot.objects.all().count(), 1)
