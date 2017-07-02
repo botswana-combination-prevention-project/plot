5c0fdb68b967026177a24eb79c65e4b21c654570
erikvw <ew2789@gmail.com>
2017-07-02 10:41:11 +0200

    minor

--- plot/settings.py
+++ plot/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'rest_framework.authtoken',
     'django_crypto_fields.apps.AppConfig',
     'django_revision.apps.AppConfig',
     'edc_base.apps.AppConfig',
