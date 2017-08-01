126742342d80579dbefe3729988a79d48a7b1805
erikvw <ew2789@gmail.com>
2017-08-01 13:53:07 +0200

    dont directly import model into util

--- plot/utils.py
+++ plot/utils.py
@@ -1,27 +1,30 @@
 from django.apps import apps as django_apps
+from django.core.exceptions import ObjectDoesNotExist
 
 from edc_base.utils import get_utcnow
 from edc_map.site_mappers import site_mappers
 
 from .constants import RESIDENTIAL_HABITABLE
-from .models import Plot
 
 
-def get_anonymous_plot(**kwargs):
+def get_anonymous_plot(plot_model=None, **kwargs):
     """Return a clinic plot or an anonymous plot.
     """
+    if not plot_model:
+        plot_model = 'plot.plot'
+    plot_model_cls = django_apps.get_model(plot_model)
     device_id = django_apps.get_app_config(
         'edc_device').device_id
     plot_identifier = django_apps.get_app_config(
         'plot').anonymous_plot_identifier
     try:
-        plot = Plot.objects.get(plot_identifier=plot_identifier)
-    except Plot.DoesNotExist:
+        plot = plot_model_cls.objects.get(plot_identifier=plot_identifier)
+    except ObjectDoesNotExist:
         lat = (site_mappers.current_mapper.center_lat
                + float('.000000{}'.format(device_id)))
         lon = (site_mappers.current_mapper.center_lon
                + float('.000000{}'.format(device_id)))
-        plot = Plot.objects.create(
+        plot = plot_model_cls.objects.create(
             plot_identifier=plot_identifier,
             report_datetime=get_utcnow(),
             map_area=site_mappers.current_map_area,
