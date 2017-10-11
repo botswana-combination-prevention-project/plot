0693025c887b28fe0ad758db3e1ccde4cefcdd9c
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-10-11 12:52:06 +0200

    create plot anonymous plot on client only not nodeserver

--- plot/utils.py
+++ plot/utils.py
@@ -1,7 +1,7 @@
 from django.apps import apps as django_apps
 from django.core.exceptions import ObjectDoesNotExist
-
 from edc_base.utils import get_utcnow
+from edc_device.constants import NODE_SERVER
 from edc_map.site_mappers import site_mappers
 
 from .constants import RESIDENTIAL_HABITABLE
@@ -10,11 +10,14 @@ from .constants import RESIDENTIAL_HABITABLE
 def get_anonymous_plot(plot_model=None, **kwargs):
     """Return a clinic plot or an anonymous plot.
     """
+    plot = None
     if not plot_model:
         plot_model = 'plot.plot'
     plot_model_cls = django_apps.get_model(plot_model)
     device_id = django_apps.get_app_config(
         'edc_device').device_id
+    device_role = django_apps.get_app_config(
+        'edc_device').device_role
     plot_identifier = django_apps.get_app_config(
         'plot').anonymous_plot_identifier
     try:
@@ -24,21 +27,22 @@ def get_anonymous_plot(plot_model=None, **kwargs):
                + float('.000000{}'.format(device_id)))
         lon = (site_mappers.current_mapper.center_lon
                + float('.000000{}'.format(device_id)))
-        plot = plot_model_cls.objects.create(
-            plot_identifier=plot_identifier,
-            report_datetime=get_utcnow(),
-            map_area=site_mappers.current_map_area,
-            description='anonymous',
-            comment='anonymous',
-            status=RESIDENTIAL_HABITABLE,
-            household_count=1,
-            eligible_members=1,
-            selected=None,
-            gps_target_lat=lat,
-            gps_target_lon=lon,
-            gps_confirmed_latitude=lat,
-            gps_confirmed_longitude=lon,
-            ess=True,
-            rss=False,
-            htc=False)
+        if not device_role == NODE_SERVER:
+            plot = plot_model_cls.objects.create(
+                plot_identifier=plot_identifier,
+                report_datetime=get_utcnow(),
+                map_area=site_mappers.current_map_area,
+                description='anonymous',
+                comment='anonymous',
+                status=RESIDENTIAL_HABITABLE,
+                household_count=1,
+                eligible_members=1,
+                selected=None,
+                gps_target_lat=lat,
+                gps_target_lon=lon,
+                gps_confirmed_latitude=lat,
+                gps_confirmed_longitude=lon,
+                ess=True,
+                rss=False,
+                htc=False)
     return plot
