56c8271b7bade952285f42308fdb51778b79224a
Itumeleng Mosweu <imosweuk@gmail.com>
2017-10-11 13:29:55 +0200

    test create anonymous plot nodeserver

--- plot/tests/test_get_anonymous_plot.py
+++ plot/tests/test_get_anonymous_plot.py
@@ -1,6 +1,7 @@
 from django.apps import apps as django_apps
-from django.test import TestCase
+from django.test import TestCase, tag
 
+from edc_device.constants import CENTRAL_SERVER, NODE_SERVER
 from edc_map.site_mappers import site_mappers
 from survey.tests import SurveyTestHelper
 
@@ -21,8 +22,21 @@ class TestCreateAnonymousPlot(TestCase):
         site_mappers.loaded = False
         site_mappers.register(TestPlotMapper)
 
+    def tearDown(self):
+        del django_apps.app_configs['edc_device'].device_id
+        django_apps.app_configs['edc_device'].device_role = CENTRAL_SERVER
+
     def test_create_anonymous_plot(self):
         """Test creating a clinic plot.
         """
         get_anonymous_plot()
         self.assertEqual(Plot.objects.all().count(), 1)
+
+    @tag('node')
+    def test_create_anonymous_plot_node_server(self):
+        """Test creating a clinic plot.
+        """
+        django_apps.app_configs['edc_device'].device_id = '89'
+        django_apps.app_configs['edc_device'].device_role = NODE_SERVER
+        get_anonymous_plot()
+        self.assertEqual(Plot.objects.all().count(), 0)
